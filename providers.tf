5778f5c7c090241fd0cc1f0666b15bfd909cb31e
sovanrout <sovankumar.rout@incture.com>
2024-09-12 18:16:43 +0530

    Commit providers.tf

--- providers.tf
+++ providers.tf
@@ -9,13 +9,12 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  profile = "Helen"
+  profile = "AWS-CLI"
   # Configuration options
 }
 
 provider "aws" {
   alias = "west"
   region = "us-west-1"
-  profile = "Helen"
+  profile = "AWS-CLI"
 }
-
